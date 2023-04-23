be4b56c0b3c9f23793fa93a17a1606592552b6dd
Milindh Vijay <milindhvijay@gmail.com>
2023-04-24 02:17:48 +0530

    Reads id from csv and test all servers

--- mtr.py
+++ mtr.py
@@ -1,4 +1,5 @@
 import time
+import csv
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,36 +8,48 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def main():
+    # Read server IDs and probe names from the CSV file
+    with open("server_name.csv") as csv_file:
+        reader = csv.DictReader(csv_file)
+        servers = list(reader)
+
+    # Get the IP address to trace
     target_ip = input("Enter the IP address to trace: ")
-    server_id = input("Enter the server ID: ")
-    url = f"https://ping.sx/mtr?t={target_ip}&p={server_id}"
 
+    # Set up Chrome options and driver
     chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
     driver = webdriver.Chrome(executable_path="path/to/chromedriver", options=chrome_options)
-    driver.maximize_window()
-    driver.get(url)
 
-    # Wait for the target input field to be interactable before pressing Enter
-    target_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'target')))
-    target_input.send_keys(Keys.RETURN)
+    for server in servers:
+        # Get the server ID and probe name
+        server_id = server['server_id']
+        probe_name = server['probe_name']
 
-    # Wait for 15 seconds to let the page load completely
-    time.sleep(15)
+        # Construct the URL for the MTR
+        url = f"https://ping.sx/mtr?t={target_ip}&p={server_id}"
 
-    # Get the probe name from the webpage
-    probe_name = driver.find_element(By.CSS_SELECTOR, 'h3.ml-2.mt-2.text-sm.leading-6.font-normal.text-gray-900').text
+        # Load the MTR in the browser
+        driver.get(url)
 
-    # Get the current date and time in the desired format
-    now = datetime.now()
-    current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+        # Wait for the target input field to be interactable before pressing Enter
+        target_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'target')))
+        target_input.send_keys(Keys.RETURN)
 
-    # Rename the screenshot file with the probe name and current date and time
-    file_name = f"{probe_name}_{target_ip}_{current_time}.png"
+        # Wait for 15 seconds to let the page load completely
+        time.sleep(15)
 
-    # Take a screenshot of the full page and save it to the specified location
-    driver.save_screenshot(f"path/to/save/{file_name}")
+        # Get the current date and time in 24h format
+        now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
+        # Take a screenshot of the full page and save it with a unique name
+        file_name = f"{probe_name}_{target_ip}_{now}.png"
+        file_path = f"path/to/{file_name}"
+        driver.save_screenshot(file_path)
+
+    # Close the browser
     driver.quit()
 
 if __name__ == "__main__":
