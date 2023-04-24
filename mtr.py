43bef0cc5570e18f4c509c572828c0150b9590f2
Milindh Vijay <milindhvijay@gmail.com>
2023-04-24 16:14:56 +0530

    Choose threads to run mtr tests

--- mtr.py
+++ mtr.py
@@ -7,7 +7,37 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from concurrent.futures import ThreadPoolExecutor
 
+def run_mtr(server_id, probe_name, target_ip):
+    # Construct the URL for the MTR
+    url = f"https://ping.sx/mtr?t={target_ip}&p={server_id}"
+
+    # Set up Chrome options and driver
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(executable_path="path/to/chromedriver", options=chrome_options)
+
+    # Load the MTR in the browser
+    driver.get(url)
+
+    # Wait for the target input field to be interactable before pressing Enter
+    target_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'target')))
+    target_input.send_keys(Keys.RETURN)
+
+    # Wait for 15 seconds to let the page load completely
+    time.sleep(15)
+
+    # Get the current date and time in 24h format
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Take a screenshot of the full page and save it with a unique name
+    file_name = f"{probe_name}_{target_ip}_{now}.png"
+    file_path = f"path/to/{file_name}"
+    driver.save_screenshot(file_path)
+
+    # Close the browser
+    driver.quit()
 
 def main():
     # Read server IDs and probe names from the CSV file
@@ -18,39 +48,21 @@ def main():
     # Get the IP address to trace
     target_ip = input("Enter the IP address to trace: ")
 
-    # Set up Chrome options and driver
-    chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(executable_path="path/to/chromedriver", options=chrome_options)
+     # Get the number of windows to run MTR tests
+    num_windows = int(input("Enter the number of windows to run MTR tests simultaneously: "))
 
-    for server in servers:
-        # Get the server ID and probe name
-        server_id = server['server_id']
-        probe_name = server['probe_name']
+    # Split the servers into batches of num_windows
+    batches = [servers[i:i+num_windows] for i in range(0, len(servers), num_windows)]
 
-        # Construct the URL for the MTR
-        url = f"https://ping.sx/mtr?t={target_ip}&p={server_id}"
+    for batch in batches:
+        # Launch num_windows threads to run the MTR for each server in the batch
+        with ThreadPoolExecutor(max_workers=num_windows) as executor:
+            for server in batch:
+                server_id = server['server_id']
+                probe_name = server['probe_name']
+                executor.submit(run_mtr, server_id, probe_name, target_ip)
 
-        # Load the MTR in the browser
-        driver.get(url)
-
-        # Wait for the target input field to be interactable before pressing Enter
-        target_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'target')))
-        target_input.send_keys(Keys.RETURN)
-
-        # Wait for 15 seconds to let the page load completely
-        time.sleep(15)
-
-        # Get the current date and time in 24h format
-        now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-        # Take a screenshot of the full page and save it with a unique name
-        file_name = f"{probe_name}_{target_ip}_{now}.png"
-        file_path = f"path/to/{file_name}"
-        driver.save_screenshot(file_path)
-
-    # Close the browser
-    driver.quit()
+    print("MTR tests complete")
 
 if __name__ == "__main__":
     main()
